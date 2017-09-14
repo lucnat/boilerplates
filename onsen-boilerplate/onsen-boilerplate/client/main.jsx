e01d761e0550cdc415ced46e9e7e57929a693014
Luca Naterop <luca.naterop@bluewin.ch>
2017-09-14 22:25:24 +0200

    onsenui + meteor boilerplate

---
+++ onsen-boilerplate/onsen-boilerplate/client/main.jsx
@@ -0,0 +1,46 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import {render} from 'react-dom';
+
+import ons from 'onsenui';
+import * as Ons from 'react-onsenui';
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
+class App extends React.Component {
+	renderToolbar() {
+		return (
+			<Ons.Toolbar>
+				<div className="center">My App</div>
+				<div className="right">
+					<Ons.ToolbarButton>
+						<Ons.Icon icon='ion-navicon, material:md-menu'></Ons.Icon>
+					</Ons.ToolbarButton>
+				</div>
+			</Ons.Toolbar>
+		);
+	}
+
+	render() {
+		return (
+			<Ons.Page renderToolbar={this.renderToolbar}>
+				<p style={{textAlign: 'center'}}>
+					<Ons.Button onClick={this.handleClick}>
+						Click me!
+					</Ons.Button>
+				</p>
+			</Ons.Page>
+		);
+	}
+
+	handleClick() {
+		ons.notification.alert('Oi dickhead');
+	}
+}
+
+Meteor.startup(() => {
+	var url = new URL(document.location.href);
+	var platform = url.searchParams.get('platform');
+	ons.platform.select(platform);
+	render(<App />, document.getElementById('render-target'));
+})
