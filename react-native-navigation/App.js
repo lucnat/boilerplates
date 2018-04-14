8a12428c00aaf15b42ef1816b01d85137d52c369
Luca Naterop <luca.naterop@bluewin.ch>
2018-04-14 12:38:57 +0200

    some more stuff

--- react-native-navigation/App.js
+++ react-native-navigation/App.js
@@ -1,73 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
-import React, { Component } from 'react';
+import React from 'react';
 import { StackNavigator } from 'react-navigation';
-import { Platform, StyleSheet, Text, View } from 'react-native';
 import * as Na from 'native-base';
 
-class HomeScreen extends Component<Props> {
-	
-	static navigationOptions(navigation){
-		return { title: 'Home' }
-	}
+class Welcome extends React.Component {
 
-  render() {
-  console.disableYellowBox = true;
-  console.log(this.props.navigation)
-    return (
-    	<Na.Container>
-    		<Na.Content padder>
-    			<Text>Watup</Text>
-          <Na.Button style={{marginTop: 10}} onPress={() => {this.props.navigation.navigate('Profile')}}>
-            <Na.Text >Go to Profile </Na.Text>
-          </Na.Button>
-    		</Na.Content>
-    	</Na.Container>
-    );
+  static navigationOptions(navigation){
+    return { title: 'Welcome' }
   }
-}
-
-class ProfileScreen extends Component<Props> {
-
-  static navigationOptions = {
-    title: 'Profile',
-    headerStyle: {
-      backgroundColor: '#f4511e',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
-  };
 
   render() {
     return (
-    	<Na.Container>
-    		<Na.Content padder>
-    			<Text>Lorem ipsum</Text>
-    		</Na.Content>
-    	</Na.Container>
+      <Na.Container>
+        <Na.Content>
+
+        </Na.Content>
+      </Na.Container>
     );
   }
 }
 
 const NavigationStack = StackNavigator({
-  Home: {
-    screen: HomeScreen,
+  'Welcome': {
+    screen: Welcome,
   },
-  Profile: {
-    screen: ProfileScreen,
-  },
-}, {
-	initialRoute: 'Home'
+  }, {
+    initialRoute: 'Welcome'
 });
 
 export default class App extends React.Component {
-	render() {
-		return <NavigationStack />
-	}
+  render() {
+    console.disableYellowBox = true;
+    return <NavigationStack />
+  }
 }
